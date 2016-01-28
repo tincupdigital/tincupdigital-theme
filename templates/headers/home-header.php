d17766b6d2206f8a7228e50c5c1bb482488ec7af
Sean McHenry <seankmchenry@gmail.com>
2016-01-27 19:01:48 -0500

    Updated flexbox nav (no hacks needed)

--- templates/headers/home-header.php
+++ templates/headers/home-header.php
@@ -8,10 +8,10 @@
 
 <header id="masthead" class="site-header" role="banner">
   <nav id="site-navigation" class="main-navigation" role="navigation">
-    <div class="column site-header--column">
+    <div class="column main-navigation--column">
       <?php wp_nav_menu( array( 'theme_location' => 'main-nav_part-1', 'menu_id' => 'main-nav_part-1' ) ); ?>
     </div>
-    <div class="column site-header--column">
+    <div class="column main-navigation--column">
       <?php wp_nav_menu( array( 'theme_location' => 'main-nav_part-2', 'menu_id' => 'main-nav_part-2' ) ); ?>
     </div>
   </nav><!-- #site-navigation -->
--- templates/headers/main-header.php
+++ templates/headers/main-header.php
@@ -8,10 +8,10 @@
 
 <header id="masthead" class="site-header" role="banner">
   <nav id="site-navigation" class="main-navigation" role="navigation">
-    <div class="column site-header--column main-nav1">
+    <div class="column main-navigation--column main-nav1">
       <?php wp_nav_menu( array( 'theme_location' => 'main-nav_part-1', 'menu_id' => 'main-nav_part-1' ) ); ?>
     </div>
-    <div class="column site-header--column">
+    <div class="column main-navigation--column">
       <div class="site-branding">
         <h1 class="site-title m0">
           <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
@@ -21,7 +21,7 @@
         <p class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></p>
       </div><!-- .site-branding -->
     </div>
-    <div class="column site-header--column">
+    <div class="column main-navigation--column">
       <?php wp_nav_menu( array( 'theme_location' => 'main-nav_part-2', 'menu_id' => 'main-nav_part-2' ) ); ?>
     </div>
   </nav><!-- #site-navigation -->
