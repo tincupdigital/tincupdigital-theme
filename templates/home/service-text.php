f3487ac03c30a50737abbaaf92600daf14c21687
Sean McHenry <seankmchenry@gmail.com>
2016-02-21 19:16:02 -0500

    Fixed styles not applied to service sections

--- templates/home/service-text.php
+++ templates/home/service-text.php
@@ -23,7 +23,7 @@ $b_lnk = get_sub_field( 'button_link' ); ?>
   <div class="service-text service-item--text">
     <?php /* Headline */
     if ( $s_hdl ) { ?>
-      <h2 class="service-headline h3 mt0"><?php echo $s_hdl; ?></h2>
+      <h2 class="service-headline service-section--headline h3 mt0"><?php echo $s_hdl; ?></h2>
     <?php }
 
     /* Text */
@@ -33,7 +33,7 @@ $b_lnk = get_sub_field( 'button_link' ); ?>
 
     /* Button */
     if ( $b_txt && $b_lnk ) { ?>
-      <a class="service-button" href="<?php echo get_permalink( $b_lnk ); ?>">
+      <a class="service-button service-section--button" href="<?php echo get_permalink( $b_lnk ); ?>">
         <?php echo $b_txt; ?>
       </a>
     <?php } ?>
