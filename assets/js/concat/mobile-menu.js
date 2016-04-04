25b358747c846fa9c8b7196eb4fe0a2a3d3a70d4
Sean McHenry <seankmchenry@gmail.com>
2016-04-04 19:08:32 -0400

    Clicking mobile button closes menu, added current page indicator

--- assets/js/concat/mobile-menu.js
+++ assets/js/concat/mobile-menu.js
@@ -23,8 +23,10 @@
   // remove class if click outside menu
   $(document).mouseup(function(e) {
     var menuArea = $('.mobile-menu .menu');
+    var menuButton = $('.menu-toggle');
+
     // check if menu area is targeted
-    if ( !menuArea.is(e.target) && ( menuArea.has(e.target).length === 0 ) ) {
+    if ( ( !menuArea.is(e.target) && !menuButton.is(e.target) ) && ( menuArea.has(e.target).length === 0 ) ) {
       $('.site').removeClass('pushed');
     }
   });
